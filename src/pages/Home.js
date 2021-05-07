b88c8b86ef9b66ca32eb1df76573cfbd847a1d28
MauriceOSullivan <mauriceyamanoi@gmail.com>
2021-05-07 19:57:12 +0100

    minor fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div className='home'>
             <h3>
-            Hi, I'm Maurice (O'Sullivan), an aspiring front-end developer.
+            Hi, I'm Maurice O'Sullivan, an aspiring front-end developer.
             I currently work at Amazon as an Analyst, and I graduated from the University of Michigan in 2019 with a degree in Economics and minor in Computer Science.
             </h3>
             <h3>
