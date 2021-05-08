06ca5e28a69be240b3d17fcffd5650681aac2b82
MauriceOSullivan <mauriceyamanoi@gmail.com>
2021-05-08 17:36:03 +0100

    improved london section

--- src/pages/London.js
+++ src/pages/London.js
@@ -2,7 +2,15 @@ import React, {useState, useEffect} from 'react'
 import Amplify, {API, graphqlOperation } from 'aws-amplify';
 import awsconfig from '../aws-exports.js';
 import {listLondonPlaces} from '../graphql/queries';
-import { getLondonPlace } from '../graphql/queries';
+import { Card } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 Amplify.configure(awsconfig);
 
 
@@ -10,28 +18,50 @@ function London() {
     const [places, setPlaces] = useState([]);
 
     useEffect(() => {
-        const fetchPlaces = async () => {
-            try {
-                const placesData = await API.graphql(graphqlOperation(listLondonPlaces));
-                const placesList = placesData.data.listLondonPlaces.items;
-                console.log('placesList', placesList);
-                getLondonPlace(placesList);
-                console.log('places list', placesList)
-                setPlaces(placesList)
-            } 
-            catch (error) {
-                console.log('error on fetching songs', error);
-            }
-        };
         fetchPlaces();
-        console.log('places array', places)
     }, [])
 
+    const fetchPlaces = async () => {
+        try {
+            const placesData = await API.graphql(graphqlOperation(listLondonPlaces));
+            const placesList = placesData.data.listLondonPlaces.items;
+            console.log('placesList', placesList);
+            setPlaces(placesList);
+        } 
+        catch (error) {
+            console.log('error on fetching songs', error);
+        }
+    };
+    const useStyles = makeStyles({
+        root: {
+          maxWidth: 345,
+        },
+        media: {
+          height: 140,
+        },
+      });
+    
+    const classes = useStyles();
     return (
-        <div className='placesList'>
-            {places.map(place => {
-                <div> Name: {place.name} Address: {place.address}</div>
-            })}
+        
+        <div>
+            {places.map(places => 
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardContent>
+                    <Typography gutterBottom variant="h6" component="h2">
+                        {places.name} 
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Address: {places.address}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Description: {places.description}
+                    </Typography>
+                    </CardContent>
+                    </CardActionArea>
+            </Card>
+            )}
         </div>
     )
 }
